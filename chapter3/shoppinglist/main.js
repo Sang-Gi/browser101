6dcc7f0079bf855b07979268900d5ddb9163be5a
SangGi <jackid1103@naver.com>
2022-04-19 14:29:23 +0900

    input 영어 추가 오류 해결

--- chapter3/shoppinglist/main.js
+++ chapter3/shoppinglist/main.js
@@ -2,6 +2,12 @@ const items = document.querySelector('.content');
 const input = document.querySelector('.itemInput');
 const itemBtn = document.querySelector('.itemBtn');
 
+/* 
+코드를 그대로 설명하는 주석은
+가독성을 떨어트릴뿐이다.
+가능한 '왜' 이 코드를 사용했는지 주석을 남길 것.
+ */
+
 function onAdd() {
     // 1. 사용자가 입력한 텍스트 받아옴.
     const text = input.value;
@@ -13,7 +19,9 @@ function onAdd() {
     const item = createItem(text);
     // 3. 컨테이너안에 새로 만든 아이템 추가.
     items.appendChild(item);
-    // 4. 인풋을 초기화 한다.
+    // 4. 새로 추가된 아이템으로 스크롤링
+    item.scrollIntoView({block : 'center'});
+    // 5. 인풋을 초기화 한다.
     input.value = '';
     input.focus();
 }
@@ -52,5 +60,7 @@ itemBtn.addEventListener('click', () => {
 });
 
 input.addEventListener('keypress', (e) => {
-    onAdd();
+    if(e.key === 'Enter') {
+        onAdd();
+    }
 })
